ffc33825031a857cd00e6f1653f587de61cea9ad
helinlacin <114211908+helinlacin@users.noreply.github.com>
2023-04-23 15:58:26 +0300

    Article,Category,Image entitileri oluşturuldu.

---
+++ BlogProject.Entity/Entities/Article.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Entity.Entities
+{
+    public class Article
+    {
+        public Guid  Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int ViewCount { get; set; }
+        public Guid CategoryId { get; set; }    
+        public Category Category { get; set; }
+
+        public Guid ImageId { get; set; }
+        public Image Image { get; set; }
+    }   
+}
